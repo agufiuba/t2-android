8cb03a9a9895650245e1e74870d18733c5d10a1a
DMaitia <dariusmaitia@gmail.com>
2017-10-03 18:04:24 -0300

    Desarrollo de la parte de modificacion de perfil

--- app/src/main/java/com/example/darius/taller_uber/MainActivity.java
+++ app/src/main/java/com/example/darius/taller_uber/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.darius.taller_uber;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -81,7 +82,8 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.perfil) {
-
+            Intent intent = new Intent(this, ProfileActivity.class);
+            startActivity(intent);
         } else if (id == R.id.chat) {
 
         } else if (id == R.id.nav_manage) {
